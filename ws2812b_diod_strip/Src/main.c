34502f5dd2b2a5fe66b3a3f0df4269bd33e7ca2b
Yulia Gapunovych <45093751+YulitaGap@users.noreply.github.com>
2020-01-21 15:49:50 +0200

    final version

--- ws2812b_diod_strip/Src/main.c
+++ ws2812b_diod_strip/Src/main.c
@@ -1,51 +1,21 @@
-/* USER CODE BEGIN Header */
+
 /**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * This notice applies to any and all portions of this file
- * that are not between comment pairs USER CODE BEGIN and
- * USER CODE END. Other portions of this file, whether
- * inserted by the user or by software development tools
- * are owned by their respective copyright owners.
- *
- * Copyright (c) 2018 STMicroelectronics International N.V.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted, provided that the following conditions are met:
- *
- * 1. Redistribution of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of STMicroelectronics nor the names of other
- *    contributors to this software may be used to endorse or promote products
- *    derived from this software without specific written permission.
- * 4. This software, including modifications and/or derivative works of this
- *    software, must execute solely and exclusively on microcontroller or
- *    microprocessor devices manufactured by or for STMicroelectronics.
- * 5. Redistribution and use of this software other than as permitted under
- *    this license is void and will automatically terminate your rights under
- *    this license.
- *
- * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
- * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
- * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -58,7 +28,9 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stdlib.h"
+#include "stm32f4xx_hal.h"
+#include "../Drivers/STM32F4Discovery/Inc/stm32f411e_discovery_accelerometer.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -68,7 +40,6 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -92,29 +63,6 @@ void MX_USB_HOST_Process(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void delay_asm(uint32_t useconds) {
-	asm volatile("   mov r0, %[useconds]    \n"
-			"1: subs r0, #1            \n"
-			"   bhi 1b                 \n": :[useconds] "r" (useconds): "r0");
-}
-
-inline void send_byte(int a) {
-	for (int i = 7; i >= 0; i--) {
-		int x = (a >> i) & 1;
-		if (x == 1) {
-			GPIOB->BSRR = GPIO_PIN_8;
-			delay_asm(20); //high_delay
-			GPIOB->BSRR = (uint32_t) GPIO_PIN_8 << 16U;
-			delay_asm(10); //low delay
-		} else {
-			GPIOB->BSRR = GPIO_PIN_8;
-			delay_asm(10); //low delay
-			GPIOB->BSRR = (uint32_t) GPIO_PIN_8 << 16U;
-			delay_asm(20);//high_delay
-
-		}
-	}
-}
 
 /* USER CODE END 0 */
 
@@ -125,7 +73,8 @@ inline void send_byte(int a) {
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	 extern void initialise_monitor_handles(void);
+	 initialise_monitor_handles();
   /* USER CODE END 1 */
   
 
@@ -154,39 +103,113 @@ int main(void)
   MX_USB_HOST_Init();
   /* USER CODE BEGIN 2 */
 
+  volatile uint8_t err_res = BSP_ACCELERO_Init();
+    if (err_res != HAL_OK)
+    {
+      HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, SET);
+      /* Initialization Error */
+      while(1){
+      }
+    }
+    void delay_asm(uint32_t useconds) {
+    	asm volatile("   mov r0, %[useconds]    \n"
+    			"1: subs r0, #1            \n"
+    			"   bhi 1b                 \n": :[useconds] "r" (useconds): "r0");
+    }
+
+    inline void send_byte(int a) {
+    	for (int i = 7; i >= 0; i--) {
+    		int x = (a >> i) & 1;
+    		if (x == 1) {
+    			GPIOB->BSRR = GPIO_PIN_8;
+    			delay_asm(20); //high_delay
+    			GPIOB->BSRR = (uint32_t) GPIO_PIN_8 << 16U;
+    			delay_asm(10); //low delay
+    		} else {
+    			GPIOB->BSRR = GPIO_PIN_8;
+    			delay_asm(10); //low delay
+    			GPIOB->BSRR = (uint32_t) GPIO_PIN_8 << 16U;
+    			delay_asm(20);//high_delay
+
+    		}
+    	}
+    }
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-	// light on all 60 diodes
-	for (int i = 0; i < 60; i++) { 
-		send_byte(0x00);
-		send_byte(0x00);
-		send_byte(0x00);
-	}
-	HAL_Delay(1);
+    int16_t buffer[3] = {0};
+    int16_t prev_buffer[3] = {0};
+    BSP_ACCELERO_GetXYZ(prev_buffer);
+    int shaking = 0;
+    int change = 0;
+    while (1)
+    {
+     BSP_ACCELERO_GetXYZ(buffer);
+     if (abs(buffer[0] - prev_buffer[0]) > 2500 || abs(buffer[1] - prev_buffer[1]) > 2500 || abs(buffer[2] - prev_buffer[2]) > 2500) {
+    	 if (!shaking) {
+    		 change = 1;
+    	 } else {
+    		 change = 0;
+    	 }
+    	 shaking = 1;
+     } else {
+    	 if (shaking) {
+    		 change = -1;
+    	 } else {
+    		 change = 0;
+    	 }
+    	 shaking = 0;
+     }
+     if (change == 1) {
+    	 for (int j = 0; j < 60; j++) {
+			 for (int i = 0; i < 60; i++) {
+											send_byte(0x00 + j);
+											send_byte(0x3C - j);
+											send_byte(0x00);
+					 }
+			 HAL_Delay(2);
+    	 }
+     } else if (change == -1) {
+    	 for (int j = 0; j < 60; j++) {
+    	 			 for (int i = 0; i < 60; i++) {
+    	 											send_byte(0x3C - j);
+    	 											send_byte(0x00 + j);
+    	 											send_byte(0x00);
+    	 					 }
+    	 			 HAL_Delay(2);
+    	     	 }
+     }
+     if (shaking) {
+		 for (int i = 0; i < 60; i++) {
+								send_byte(0x3C);
+								send_byte(0x00);
+								send_byte(0x00);
+		 }
+     } else {
+    	 for (int i = 0; i < 60; i++) {
+    	 						send_byte(0x00);
+    	 						send_byte(0x3C);
+    	 						send_byte(0x00);
+    	 }
 
-	while (1) {
-		for (int i = 0; i < 20; i++) {
-			// ligth on all diodes one by one
-			if(i%2 == 0){
-				send_byte(0x00);
-				send_byte(0xFF);
-				send_byte(0x00);
-			}
-			else{
-			send_byte(0x00);
-			send_byte(0xFF);
-			send_byte(0xFF);
-			}
-		}
-		HAL_Delay(1);
+     }
 
-		MX_USB_HOST_Process();
-	}
+     for (int i = 0; i < 3; i++) {
+    	 prev_buffer[i] = buffer[i];
+     }
+
+     HAL_Delay(300);
+
+    /* USER CODE END WHILE */
+    MX_USB_HOST_Process();
+
+    /* USER CODE BEGIN 3 */
+
 
   /* USER CODE END 3 */
-}
+}}
 
 /**
   * @brief System Clock Configuration
@@ -250,7 +273,7 @@ void SystemClock_Config(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-	/* User can add his own implementation to report the HAL error return state */
+  /* User can add his own implementation to report the HAL error return state */
 
   /* USER CODE END Error_Handler_Debug */
 }
@@ -266,12 +289,10 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 { 
   /* USER CODE BEGIN 6 */
-	/* User can add his own implementation to report the file name and line number,
-	 tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
-
-
